3e492702d43e89dd22c0abab04f69663a95671b5
Sam Brown <jumpbro@hotmail.com>
2023-08-06 11:16:04 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AllRecipesPage from './pages/AllRecipesPage';
 import RecipesToTryPage from './pages/RecipesToTryPage';
 import FavouriteRecipesPage from './pages/FavouriteRecipesPage';
@@ -15,20 +15,22 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App/>}>
-        <Route path="/recipes/:id" element={<Recipe />}/>
-        <Route path="/all-recipes" element={<AllRecipesPage />}/>
-        <Route path="/recipes-to-try" element={<RecipesToTryPage />}/>
-        <Route path="/favourites" element={<FavouriteRecipesPage />}/>
-        <Route path="/failed" element={<FailedRecipesPage />}/>
-        <Route path="/recipes-to-reattempt" element={<RecipesToReattemptPage />}/>
-
+      <Route path='/' element={<App />}>
+        <Route path='/recipes/:id' element={<Recipe />} />
+        <Route path='/all-recipes' element={<AllRecipesPage />} />
+        <Route path='/recipes-to-try' element={<RecipesToTryPage />} />
+        <Route path='/favourites' element={<FavouriteRecipesPage />} />
+        <Route path='/failed' element={<FailedRecipesPage />} />
+        <Route
+          path='/recipes-to-reattempt'
+          element={<RecipesToReattemptPage />}
+        />
       </Route>
     </Routes>
-</BrowserRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
